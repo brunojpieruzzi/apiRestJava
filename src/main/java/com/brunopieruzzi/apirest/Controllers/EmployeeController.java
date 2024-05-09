840a48607d5485dc385c3966aa0051503ffc4419
brunoieruzzi <pieruzzib@gmail.com>
2024-05-08 21:30:00 -0300

    correxion de codigo para nuevas implementaciones

--- src/main/java/com/brunopieruzzi/apirest/Controllers/EmployeeController.java
+++ src/main/java/com/brunopieruzzi/apirest/Controllers/EmployeeController.java
@@ -3,7 +3,7 @@ package com.brunopieruzzi.apirest.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.brunopieruzzi.apirest.Model.Employee;
 import com.brunopieruzzi.apirest.Repositories.EmployeeRepository;
-
 
 @RestController
 @RequestMapping("/empleados")
@@ -32,19 +30,19 @@ public class EmployeeController {
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable String id) {
         return employeeRepository.findById(id)
-                                .orElseThrow(() -> new RuntimeException("Employee not found"));
+                .orElseThrow(() -> new RuntimeException("Employee not found"));
     }
 
     @PostMapping()
-    public Employee createEmploye(@RequestBody Employee employee){
+    public Employee createEmploye(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
 
     @PutMapping()
-    public Employee upDatEmployee(@PathVariable String id, @RequestBody Employee emp){
-        
+    public Employee upDatEmployee(@PathVariable String id, @RequestBody Employee emp) {
+
         Employee employee = employeeRepository.findById(id)
-        .orElseThrow(() -> new RuntimeException("Employee not found"));
+                .orElseThrow(() -> new RuntimeException("Employee not found"));
 
         employee.setName(emp.getName());
         employee.setSurname(emp.getSurname());
@@ -52,5 +50,13 @@ public class EmployeeController {
 
         return employeeRepository.save(employee);
     }
-}
 
+    @DeleteMapping("/{id}")
+    public String deleteEmployee(@PathVariable String id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee not found"));
+
+        employeeRepository.delete(employee);
+        return "el empleado a sido borrado";
+    }
+}
